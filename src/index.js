34f4b56db657a12ac74c60a2ca05545ead74e6e2
Slava Bezgachev <bezgachev@gmail.com>
2019-01-28 17:55:54 +0600

    Basic files

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>I'm the app</div>;
+};
+
+const app = document.getElementById("app");
+
+ReactDOM.render(<App />, app);
