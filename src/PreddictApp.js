74bc7ae5e548240134bd5b00bf9e19daf45f68e8
Slava Bezgachev <bezgachev@gmail.com>
2019-01-30 16:33:01 +0600

    Added styled components and react router

--- src/PreddictApp.js
+++ src/PreddictApp.js
@@ -1,11 +1,29 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+const MainPage = () => <div>Main Page</div>;
+const Watching = () => <div>Watching Page</div>;
 
 class PreddictApp extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Howdy pardner! {`\u{1F920}`}</p>
-      </div>
+      <Router className="App">
+        <div>
+          <p>Howdy pardner! {`\u{1F920}`}</p>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/watching">Watching</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/watching" component={Watching} />
+            <Route path="/" component={MainPage} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
