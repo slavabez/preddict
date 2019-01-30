bf24692fa725da77efa6a45b17a9fdb7fdb5e29b
Slava Bezgachev <bezgachev@gmail.com>
2019-01-30 16:19:17 +0600

    Removed cra stuff

---
+++ src/PreddictApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class PreddictApp extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Howdy pardner! {`\u{1F920}`}</p>
+      </div>
+    );
+  }
+}
+
+export default PreddictApp;
